5f7175afa19e5baa6c7b58ab3c7f01f434c09da6
muhammadrasul2007 <124247331+muhammadrasul2007@users.noreply.github.com>
2023-02-14 08:49:27 +0500

    Update 1-dart.dart

--- 1-dart.dart
+++ 1-dart.dart
@@ -1,1 +1,10 @@
-print "Hello world"
+// Define a function.
+void printInteger(int aNumber) {
+  print('The number is $aNumber.'); // Print to console.
+}
+
+// This is where the app starts executing.
+void main() {
+  var number = 42; // Declare and initialize a variable.
+  printInteger(number); // Call a function.
+}
